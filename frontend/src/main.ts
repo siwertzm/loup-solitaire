73c641a179b9a71d3a5de7b875b9dc48af7b8c43
marius siwertz <siwertzm@gmail.com>
2025-03-27 20:42:45 +0100

    login test

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { importProvidersFrom } from '@angular/core'; // 👈 à ajouter
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    importProvidersFrom(HttpClientModule) // ✅ correct ici
   ],
 });
