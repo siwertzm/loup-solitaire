1f88805fe0c3b15c69f7f69d153a548f37d5640b
marius siwertz <siwertzm@gmail.com>
2025-03-27 14:11:16 +0100

    ok derniere erreur de style nonnull

--- backend/src/main/java/com/loupsolitaire/backend/config/JwtFilter.java
+++ backend/src/main/java/com/loupsolitaire/backend/config/JwtFilter.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import org.springframework.lang.NonNull;
 
 @Component
 @RequiredArgsConstructor
@@ -22,9 +23,9 @@ public class JwtFilter extends OncePerRequestFilter {
     private final CustomUserDetailsService userDetailsService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
 
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
